dc08afdb9e99e0940190d1631a4a1e38cf936013
rou-bai <2802607350@qq.com>
2021-03-04 20:35:42 +0800

    增加服务提供者order的启动类

---
+++ order/src/main/java/com/southwind/controller/OrderHandler.java
@@ -0,0 +1,20 @@
+package com.southwind.controller.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderHandler {
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping("/index")
+    public String index(){
+        return "order端口是：" + this.port;
+    }
+
+
+}
